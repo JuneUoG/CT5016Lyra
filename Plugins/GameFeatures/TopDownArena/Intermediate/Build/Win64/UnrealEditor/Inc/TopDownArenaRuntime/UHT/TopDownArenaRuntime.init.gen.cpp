73f9c1d8099e198753b82fbc451eb958604a07b9
James Bradbury <unkoun98@gmail.com>
2025-08-21 14:56:23 +0100

    fixed for 5.6

--- Plugins/GameFeatures/TopDownArena/Intermediate/Build/Win64/UnrealEditor/Inc/TopDownArenaRuntime/UHT/TopDownArenaRuntime.init.gen.cpp
+++ Plugins/GameFeatures/TopDownArena/Intermediate/Build/Win64/UnrealEditor/Inc/TopDownArenaRuntime/UHT/TopDownArenaRuntime.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeTopDownArenaRuntime_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x961D7F14,
+				0x580F9549,
 				0xAE0CFA0A,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeTopDownArenaRuntime_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_TopDownArenaRuntime.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_TopDownArenaRuntime(Z_Construct_UPackage__Script_TopDownArenaRuntime, TEXT("/Script/TopDownArenaRuntime"), Z_Registration_Info_UPackage__Script_TopDownArenaRuntime, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x961D7F14, 0xAE0CFA0A));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_TopDownArenaRuntime(Z_Construct_UPackage__Script_TopDownArenaRuntime, TEXT("/Script/TopDownArenaRuntime"), Z_Registration_Info_UPackage__Script_TopDownArenaRuntime, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x580F9549, 0xAE0CFA0A));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
